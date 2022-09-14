ba09d6d79da640e57854187089b77f5f28c4ce20
ASUS <ASUS@DESKTOP-5LUIJ39>
2022-09-14 06:43:50 +0300

    Initial Commit

---
+++ IntegerDivider/src/main/java/ua/foxminded/javaspring/IntegerDivider.java
@@ -0,0 +1,18 @@
+package ua.foxminded.javaspring;
+
+import java.util.Scanner;
+
+public class IntegerDivider {
+
+	public static void integerDivider() {
+		Scanner scanner = new Scanner(System.in);
+		int input = scanner.nextInt();
+		System.out.println(input);
+	}
+
+	public static void main(String[] args) {
+		integerDivider();
+
+	}
+
+}
