2d60ae3ba1014457f6892381b3928eb01c274efd
ASUS <ASUS@DESKTOP-5LUIJ39>
2022-09-20 07:29:47 +0300

    Corrected according to the comments

--- IntegerDivider/src/main/java/ua/foxminded/javaspring/IntegerDivider.java
+++ IntegerDivider/src/main/java/ua/foxminded/javaspring/IntegerDivider.java
@@ -9,57 +9,77 @@ public class IntegerDivider {
 		if (divisor == 0) {
 			throw new IllegalArgumentException("Can't divide by zero");
 		} else if (divident == 0) {
-			return (new StringBuilder().append("_" + 0 + "\t|" + divisor + "\n " + 0 + "\t|-" + "\n\t|" + 0).toString());
+			String DIVIDENT_ZERO_RESULT = "_0" + "\t|" + divisor + "\n " + 0 + "\t|-" + "\n\t|0";
+			return DIVIDENT_ZERO_RESULT;
+
 		} else if (divident < divisor) {
 			throw new IllegalArgumentException("Divident must be greater than divisor");
 		} else {
 
-			String dividentString = Integer.toString(divident);
-			int[] dividentArr = new int[dividentString.length()];
-			for (int i = 0; i < dividentString.length(); i++) {
-				dividentArr[i] = dividentString.charAt(i) - '0';
-			}
-
-			Integer initialTempDivident = dividentArr[0];
-			while (initialTempDivident < divisor) {
-				initialTempDivident = initialTempDivident * 10 + dividentArr[initialTempDivident.toString().length()];
-			}
-
+			int[] dividentArr = createDigitsArray(divident);
+			Integer initialTempDivident = calculateNextDivident(divisor, dividentArr);
 			int quotient = initialTempDivident / divisor;
-			int digitAddedToQuotient = 0;
+			int nexQuotientDigit = 0;
 			int tempDivident = initialTempDivident;
 
-			StringBuilder[] blocksArr = new StringBuilder[dividentArr.length];
+			StringBuilder[] divisionBlocks = new StringBuilder[dividentArr.length - initialTempDivident.toString().length()+1];
 
-			blocksArr[0] = new StringBuilder().append("_" + divident + "\t|" + divisor + "\n " + (quotient * divisor)
-					+ "\t|-----" + "\n "
-					+ String.join("", Collections.nCopies((initialTempDivident.toString().length()), "-")) + "\t|");
+			buildFirstDivisionBlock(divident, divisor, initialTempDivident, quotient, divisionBlocks);
 
 			for (int i = 1; i <= (dividentArr.length - initialTempDivident.toString().length()); i++) {
 				tempDivident = tempDivident % divisor * 10
 						+ (dividentArr[initialTempDivident.toString().length() + i - 1]);
-				digitAddedToQuotient = tempDivident / divisor;
-				quotient = quotient * 10 + digitAddedToQuotient;
-				blocksArr[i] = new StringBuilder().append(String.join("", Collections.nCopies(i - 1, " ")) + "_"
-						+ tempDivident + "\n " + String.join("", Collections.nCopies(i - 1, " "))
-						+ (digitAddedToQuotient * divisor) + "\n" + String.join("", Collections.nCopies(i, " "))
-						+ String.join("", Collections.nCopies((initialTempDivident.toString().length() + 1), "-")));
+				nexQuotientDigit = tempDivident / divisor;
+				buildDivisionBlock(divisor, initialTempDivident, nexQuotientDigit, tempDivident, divisionBlocks, i);
+				quotient = quotient * 10 + nexQuotientDigit;
 			}
 
-			blocksArr[0].append(quotient);
+			divisionBlocks[0].append(quotient);
 
 			StringBuilder output = new StringBuilder();
 
-			for (int i = 0; i <= (dividentArr.length - initialTempDivident.toString().length()); i++) {
-				output = output.append(blocksArr[i] + "\n");
+			for (StringBuilder block : divisionBlocks) {
+				output = output.append(block + "\n");
 			}
-			output = output.append(String.join("", Collections.nCopies((dividentArr.length), " ")) + (tempDivident % divisor));
+			output = output
+					.append(String.join("", Collections.nCopies((dividentArr.length), " ")) + (tempDivident % divisor));
 
 			return output.toString();
 
-
 		}
 
+	}
+
+	private static void buildFirstDivisionBlock(int divident, int divisor, Integer initialTempDivident, int quotient,
+			StringBuilder[] divisionBlocks) {
+		divisionBlocks[0] = new StringBuilder()
+				.append("_" + divident + "\t|" + divisor + "\n " + (quotient * divisor) + "\t|-----" + "\n "
+						+ String.join("", Collections.nCopies((initialTempDivident.toString().length()), "-")) + "\t|");
+	}
+
+	private static void buildDivisionBlock(int divisor, Integer initialTempDivident, int digitAddedToQuotient,
+			int tempDivident, StringBuilder[] divisionBlocks, int i) {
+		divisionBlocks[i] = new StringBuilder().append(String.join("", Collections.nCopies(i - 1, " ")) + "_"
+				+ tempDivident + "\n " + String.join("", Collections.nCopies(i - 1, " "))
+				+ (digitAddedToQuotient * divisor) + "\n" + String.join("", Collections.nCopies(i, " "))
+				+ String.join("", Collections.nCopies((initialTempDivident.toString().length() + 1), "-")));
+	}
+
+	private static Integer calculateNextDivident(int divisor, int[] dividentArr) {
+		Integer initialTempDivident = dividentArr[0];
+		while (initialTempDivident < divisor) {
+			initialTempDivident = initialTempDivident * 10 + dividentArr[initialTempDivident.toString().length()];
+		}
+		return initialTempDivident;
+	}
+
+	private static int[] createDigitsArray(int divident) {
+		String dividentString = Integer.toString(divident);
+		int[] dividentArr = new int[dividentString.length()];
+		for (int i = 0; i < dividentString.length(); i++) {
+			dividentArr[i] = dividentString.charAt(i) - '0';
+		}
+		return dividentArr;
 	}
 
 	public static void main(String[] args) {
